66467d52ff2349db53bfdbe3bc08657fa6667f58
Finn Korol <korolodf@mcmaster.ca>
2023-02-15 16:26:50 -0500

    Initial commit

---
+++ 00-simulation.R
@@ -0,0 +1,11 @@
+seed = 69
+
+wave = sample(x = 1:4, size = 5, replace = TRUE)
+age = sample(x = 1:90, size = 5, replace = TRUE)
+
+#important survey scores
+only_means = sample(x = 1:5, size = 5, replace = TRUE)
+condemn = sample(x = 1:5, size = 5, replace = TRUE)
+justified = sample(x = 1:5, size = 5, replace = TRUE)
+message = sample(x = 1:5, size = 5, replace = TRUE)
+prevent = sample(x = 1:5, size = 5, replace = TRUE)
