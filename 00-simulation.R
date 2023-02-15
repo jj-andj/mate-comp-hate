fc6ce2072f418c60e8454050c529e469abff7346
Finn Korol <korolodf@mcmaster.ca>
2023-02-15 16:42:12 -0500

    update to simulation script

--- 00-simulation.R
+++ 00-simulation.R
@@ -1,5 +1,6 @@
-seed = 69
+set.seed(3)
 
+#participant values used in analysis in relation to survey scores
 wave = sample(x = 1:4, size = 5, replace = TRUE)
 age = sample(x = 1:90, size = 5, replace = TRUE)
 
@@ -9,3 +10,13 @@ condemn = sample(x = 1:5, size = 5, replace = TRUE)
 justified = sample(x = 1:5, size = 5, replace = TRUE)
 message = sample(x = 1:5, size = 5, replace = TRUE)
 prevent = sample(x = 1:5, size = 5, replace = TRUE)
+
+df_s = data.frame(
+  wave = wave,
+  age = age,
+  only_means = only_means,
+  condemn = condemn,
+  justified = justified,
+  message = message,
+  prevent = prevent
+  )
