a30abc90116182123e9dab9f2e9ef804fff83c98
Finn Korol <korolodf@mcmaster.ca>
2023-02-16 18:41:29 -0500

    removing more variables

--- scripts/03-clean.R
+++ scripts/03-clean.R
@@ -15,7 +15,7 @@ datc <- read_csv(here::here("inputs/data/survey.csv"), show_col_types = FALSE)
 #limiting the data to just 4th wave surveys
 #14,44, east, res_id, 
 dat_use <- datc[datc$wave == 4, ] |>
-  subset(select = -c(res_id, east, singdivsep, refugee_ind))
+  subset(select = -c(res_id, east, singdivsep, refugee_ind, list))
 
 #writing to csv
 write_csv(dat_use, here::here("inputs/data/survey_W4.csv"))
