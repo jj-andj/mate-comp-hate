b91c085ad6a7b7241f3a04c1b58f0436f5e430ce
Finn Korol <korolodf@mcmaster.ca>
2023-02-16 21:28:17 -0500

    renaming cols

--- scripts/03-clean.R
+++ scripts/03-clean.R
@@ -10,12 +10,24 @@
 library(readr)
 library(dplyr)
 library(janitor)
-datc <- read_csv(here::here("inputs/data/survey.csv"), show_col_types = FALSE)
+survey <- read_csv(here::here("inputs/data/survey.csv"), show_col_types = FALSE)
 
 #limiting the data to just 4th wave surveys
-dat_use <- datc[(datc$wave == 4 | datc$wave == 2), ] |>
-  subset(select = -c(res_id, east, singdivsep, refugee_ind, list, women_role))
-  rename(only_means = )
-
+survey_reduced_renamed <- datc[(datc$wave == 4 | datc$wave == 2), ] |>
+  subset(select = -c(res_id, east, singdivsep, refugee_ind, list, women_role)) |>
+  rename(only_means = hate_violence_means,
+         message = hate_pol_message,
+         prevent = hate_prevent_settlement,
+         condemn = hate_polcondemn,
+         justified = hate_justified,
+         mate_compete = MateComp.cont,
+         job_compete = JobComp.cont,
+         life_satisfaction = LifeSatis.cont,
+         excess_males = pop_15_44_muni_gendergap_2015,
+         muslim_aff = muslim_ind,
+         afd_aff = afd_ind,
+         contact_aff = contact_ind
+         )
+  
 #writing to csv
-write_csv(dat_use, here::here("inputs/data/survey_clean.csv"))
+write_csv(survey_reduced_renamed, here::here("inputs/data/survey_clean.csv"))
